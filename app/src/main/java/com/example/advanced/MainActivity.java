94b0b4546fbe3000a2354604694c22e43c9cdb52
milanxiaotiejiang <765151629@qq.com>
2020-03-31 09:14:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/advanced/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.advanced;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.lang.annotation.Native;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
